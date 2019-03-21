5936b14cb0538d3bbcfbb952be7292bfbd4b284c
dongchenxi <dongchenxi@baidu.com>
2019-03-21 15:15:36 +0800

    update

--- autoscript.py
+++ autoscript.py
@@ -1,32 +1,84 @@
 import wda
 import time
 
-wda.DEBUG = False # default False
-wda.HTTP_TIMEOUT = 60.0 # default 60.0 seconds
-
+# default False
 # Enable debug will see http Request and Response
 # wda.DEBUG = True
+wda.DEBUG = False 
+# default 60.0 seconds
+wda.HTTP_TIMEOUT = 60.0 
+
+# build connection
 c = wda.Client('http://localhost:8100')
+
 # Show status
 print (c.status())
 
+# instance
 s = c.session()
 e = s(type='TextField')
 
-words = ["曼联\n", "阿森纳\n", "切尔西\n", "热刺\n", "曼城\n", "利物浦\n", "波尔图\n", "大巴黎\n", "巴萨\n", "皇马\n", "拜仁\n"]
+# const
+words = ["123\n", "新浪\n", "mip\n", "秋葵的做法大全\n", "海草舞\n", 
+		 "曼联\n", "阿森纳\n", "切尔西\n", "热刺\n", "曼城\n", "利物浦\n", "波尔图\n", "大巴黎\n", "巴塞罗那\n", "皇马\n", "拜仁\n",
+         "尤文图斯\n", "都灵\n", "AC米兰\n", "罗马\n", "威尼斯\n", "桑普多利亚\n", "乌迪内斯\n", "那不勒斯\n", "阿贾克斯\n", 
+         "费耶诺德\n", "埃因霍温\n", "塞维利亚\n", "西班牙人\n", "拉齐奥\n", "法兰克福\n", "多特蒙德\n", "马德里竞技\n", "凯尔特人\n",
+         "费内巴切\n", "狼堡\n", "霍芬海姆\n", "海牙\n", "里斯本竞技\n", "热那亚\n", "尼斯\n", "里昂\n", "南特\n", 
+         "贝尔格莱德\n", "雷丁\n", "诺丁汉\n", "莫尔德\n", "苏黎世\n", "柏林赫塔\n", "博尔顿\n", "圣保罗\n", "科特布斯\n"]
+
 sec = [0, 1, 2,  3, 4, 5, 6]
 
-count  = 0
-while 1:
-	index = count % len(words)
-	key = words[index]
-	e.clear_text()
-	e.set_text(key)
-	print(str(count)+key)
-	time.sleep(3)
-	c.screenshot('img/'+str(count)+'.png')
-	count += 1
 
+def waiting():
+	time.sleep(1)
+
+
+def inputKeywords(key):
+	e.clear_text()
+
+	for w in key:
+		e.set_text(w)
+		pass
+
+
+def output(count):
+	waiting()
+	c.screenshot('img/'+str(count)+'.png')
+
+
+
+def main():
+	count  = 0
+
+	while True:
+
+		if count > 0 and count%3 == 0:
+			e.click()
+			if s(type='Cell').exists:
+				s(type='Cell', index=0).tap()
+				pass
+			pass
+
+		else:
+			index = count % len(words)
+			key = words[index]
+			inputKeywords(key)
+			print(str(count)+key)
+			pass
+
+		output(count)
+		count += 1
+
+		if count % 10 == 0:
+			if s(name='回首页').exists:
+				s(name='回首页').tap();
+				pass
+			pass
+
+		pass
+
+if __name__ == '__main__':
+    main()
 
 # s(type='TextField').set_text('曼联\n')
 # time.sleep(3)
--- readme.md
+++ readme.md
@@ -1,13 +1,57 @@
-+ 安装node/npm -- brew install node
 
-+ git clone https://github.com/facebook/WebDriverAgent.git
+#### 环境准备
 
-+ 安装 Carthage -- brew install Carthage
-    + wda 脚本通过 Carthage 安装所需依赖
++ xcode9.0 以上
 
-+ cd WebDriverAgent
-+ ./Scripts/bootstrap.sh
++ homebrew (应该都有，没有的话如下安装)
+  xcode-select --install
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-+ 端口转发 -- brew install libimobiledevice
-+ iproxy 8100 8100
-+ http://localhost:8100/inspector
++ brew install node -- 安装 node/npm
+
++ brew install Carthage -- 安装 Carthage
+
++ brew install libimobiledevice -- 真机端口转发所需依赖
+
++ brew install python3 -- 安装python3
+
++ pip3 install --pre facebook-wda -- 安装python依赖库
+
+#### 真机安装wda
+
++ git clone https://github.com/facebook/WebDriverAgent.git -- 克隆WDA工程到本地
+
++ cd WebDriverAgent - 切换到工程目录
+
++ ./Scripts/bootstrap.sh - 运行脚本，一定要在WebDriverAgent目录下运行
+
++ 用xcode打开WebDriverAgent.xcodeproj
+
++ Target 选择 WebDriverAgentRunner
+
++ 设置证书，用免费开发者证书即可
+
++ 如果用免费开发者证书，bundleId需要增加一个任意后缀，保证唯一即可
+
++ destination 选择真机
+
++ product -> test
+
++ xcode命令行能看到ip地址表示运行成功
+
+
+#### 运行脚本
+
++ iproxy 8100 8100 --> 端口转发- 运行成功命令行会提示waiting connection
+
++ 浏览器 打开 http://localhost:8100/inspector， 能看到终端界面的映射
+
++ git clone https://github.com/dongchx/wda.git -- 克隆本工程到本地
+
++ cd wda --  切换到工程目录
+
++ python3 autoscript.py -- 运行python脚本
+
+#### 其他
+
++ 屏幕截图存放在wda/img 目录下
